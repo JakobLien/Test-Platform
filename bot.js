1623975eb2308907387ba5c04df70e034570bfbc
jlien11 <jlien11@hotmail.no>
2019-01-05 18:10:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
 //The good stuff: 
 //send message: message.channel.send("");
 //reply to command message.reply("");
+//access a variable
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const https = require('https');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
+
+function getSpellData(number){
+    https.get('http://dnd5eapi.co/api/spells/'+number, (resp) => {
+        let data = '';
+        
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            return JSON.parse(data);
+        });
+        
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    
+}
 
 var i = false;
 client.on('message', message => {
@@ -21,7 +43,7 @@ client.on('message', message => {
         i = false;
     }else if(message.content === "!print"){
         message.reply(i);
-    }else if(message.content === "!test1"){
+    }else if(message.content === "!spell"){
         console.log("Im here now")
         message.channel.send("did this get through?")
     }
