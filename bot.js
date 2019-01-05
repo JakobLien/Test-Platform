6265a35e96ff200c121f9b6d9a25c1162aa963c7
jlien11 <jlien11@hotmail.no>
2019-01-05 21:26:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,8 +22,7 @@ function getSpellData(number){
         
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-            console.log(data);
-            return data;
+            message.reply(data);
         });
         
         }).on("error", (err) => {
@@ -53,7 +52,7 @@ client.on('message', message => {
                 message.reply(i);
                 break;
             case "spell":
-                message.reply(getSpellData(Number(command[1])));
+                getSpellData(Number(command[1]));
                 break;
         }
     }
