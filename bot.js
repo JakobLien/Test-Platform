5ae5a6dcf8bfea8e3532a3938ed2a00ba8c23785
jlien11 <jlien11@hotmail.no>
2019-01-05 16:39:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,24 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
- 
-
+var i = false;
 client.on('message', message => {
-
     if (message.content === '!trist') {
-
-       message.reply('Jakob er trist!');
-
-       }
-
+        message.reply('Jakob er trist!');
+    }else if(message.content === "!turn_on"){
+        message.reply("turned on");
+        i = true;
+    }else if(message.content === "!turn_off"){
+        message.reply("turned on");
+        i = false;
+    }else if(message.content === "!print"){
+        message.reply(i);
+    }
 });
-
- 
 
 // THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
