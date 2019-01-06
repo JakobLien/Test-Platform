27e85445a47c3b3a15f7f6164bf18f1354431f05
jlien11 <jlien11@hotmail.no>
2019-01-06 01:50:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,7 @@ function getSpellData(spellName){
         
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
+            console.log(data);
             last_message_object.reply(data);
         });
         
