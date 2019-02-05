145ef88353e8e6a6aa1ce78b964539744ba77740
jlien11 <jlien11@hotmail.no>
2019-02-05 09:39:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ function tellMe(message){
 }
 
 //valid commands
-const publicCommands = ["trist", "nut", "backmeup", "spell", "openPM"];
+const publicCommands = ["trist", "nut", "backmeup", "spell", "openPM", "fish"];
 const privateCommands = ["me", "us", "start", "stop", "suicide", "runSQL", "addReply", "test"];
 
 //controll variables
