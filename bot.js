4cc8fcdde79ccaca998139ae0d185785b8ab4653
jlien11 <jlien11@hotmail.no>
2019-03-15 09:47:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ function getSpellData(spellName){
 
 //stuff to print spell requests
 function formatSpellData(data){
-	console.log(typeof(data), data);
+	console.log(typeof(data), data, data["ritual"]);
 	let info = ""
 	if(data["concentration"] === "no"){con = "not "}else{let con = ""}
 	if(data["ritual"] === "no"){ritual = "not "}else{let ritual = ""}
