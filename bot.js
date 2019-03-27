5c6a9a2cb156fd33afd17671c0dca78038f27e1f
jlien11 <jlien11@hotmail.no>
2019-03-27 12:32:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,11 +73,8 @@ function sendhttpRequest(link, options = false){
 }
 
 function sendhttpsRequest(link, options = false){
-	console.log("Got here 1");
 	return new Promise(function(resolve, reject){
-		console.log("Got here 1.5");
 		if(!options){
-			console.log("Got here -1.5");
 			https.get(link, (resp) => {
 				let data = '';
 				resp.on('data', (chunk) => {
@@ -91,10 +88,12 @@ function sendhttpsRequest(link, options = false){
 				console.log("https error 3: " + err.message);
 			});
 		}else{
-			console.log("Got here 1.75");
+			console.log("Got here 1");
 			https.request(options, (resp) => {
+				console.log("Got here 1.5");
 				let data = '';
 				resp.on('data', (chunk) => {
+					console.log("Spam");
 					data += chunk;
 				});
 				resp.on('end', () => {
