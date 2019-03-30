394854e5e91841d6d447c0da6d3f6330c96689bd
jlien11 <jlien11@hotmail.no>
2019-03-30 17:47:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,7 +353,7 @@ client.on('message', message => {
 					*/
 					
 					sendhttpsRequest({host: "NasaAPIdimasV1.p.rapidapi.com",
-							  path: "/getEarthImagery", method: "POST",
+							  path: "/getEarthImagery?coordinate=50.37,26.56", method: "POST",
 							  headers: {"X-RapidAPI-Key": "bb17e77c02mshcfda7d104f3aa6ep13011djsn3ade2fc0025b",
 							  "Content-Type": "application/x-www-form-urlencoded"}}).then(returned => {
 						console.log(returned);
