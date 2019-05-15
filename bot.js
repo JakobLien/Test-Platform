8810c72d1ebc16a821316e68171bc8e125885151
jlien11 <jlien11@hotmail.no>
2019-05-15 08:45:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,7 +275,7 @@ client.on('message', message => {
 					break;
 				case "picOfTheDay":
 					sendhttpsRequest({host: "api.nasa.gov",
-							  path: "/planetary/apod?api_key=hZLeoMoQXjEAaUqCzM2ZGmaylTCkND4oMCnGxuPD", 
+							  path: "/planetary/apod", 
 							  headers: {"api_key": "hZLeoMoQXjEAaUqCzM2ZGmaylTCkND4oMCnGxuPD"}, 
 							  method: "GET"}).then(returned => {
 						console.log(returned);
