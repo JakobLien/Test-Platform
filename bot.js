3b9e1d0b884051a3c393253f8236c916d652695c
jlien11 <jlien11@hotmail.no>
2019-05-15 08:05:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //add to guild: https://discordapp.com/oauth2/authorize?&client_id=530439718823788544&scope=bot&permissions=8
 //programming idead: a todo list like Nils has, hangman, tic tac toe(AI?), a text based game like in BIG???, his noen nevne depressed
 //poppe den ut ett inspirational quote, ett roast system så den velge ut en bra roast fra databasen og sikte den på den du @-e
-//GJØR TA HER: LAG REMINDER DER NOEN SKRIV MELDING OG DATO
+//definisjon for ord. 
 
 //Important genereral stuff
 const Discord = require('discord.js');
@@ -281,30 +281,10 @@ client.on('message', message => {
 						message.reply(returned.contextWrites.to.title+"\n"+
 							      returned.contextWrites.to.explanation+"\n"+
 							      returned.contextWrites.to.url);
+						console.log(returned, returned.conextWrites);
 					});
 					break;
 				//argumented
-				case "getPicOnCords":
-					let lat, lon;
-					if(command[1] === "random"){
-						lat = Math.floor(Math.random()*90);
-						lon = Math.floor(Math.random()*90);
-					}else{
-						lat = command[1];
-						lon = command[2];
-					}
-					sendhttpsRequest({host: "earth-imagery-api.herokuapp.com", 
-							  path: "/earth/imagery/?lat="+lat+"&lon="+lon, 
-							  method: "GET"}).then(returned => {
-						if(returned["msg"] !== "Internal Service Error"){
-							message.reply(returned["url"]);
-						}else{
-							message.reply("Epic fail...\nThe cords were "+lat+" and "+lon);
-						}
-					}).catch(e => {
-						console.log("Error with getPicOnCords: "+e);
-					});
-					break;
 				case "spell":
 					getSpellThings(command.slice(1).join("+")).then(returned => 
 						splitText(returned).forEach(function(item){
