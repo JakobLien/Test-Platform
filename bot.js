f637539623c3a1c735e7b5301f6ebbae0f130a23
jlien11 <jlien11@hotmail.no>
2019-05-15 08:13:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,7 +274,7 @@ client.on('message', message => {
 					}
 					break;
 				case "picOfTheDay":
-					sendhttpsRequest({host: "NasaAPIdimasV1.p.rapidapi.com",
+					sendhttpsRequest({host: "earth-imagery-api.herokuapp.com",
 							  path: "/getPictureOfTheDay", method: "POST",
 							  headers: {"X-RapidAPI-Key": "bb17e77c02mshcfda7d104f3aa6ep13011djsn3ade2fc0025b",
 							  "Content-Type": "application/x-www-form-urlencoded"}}).then(returned => {
