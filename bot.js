fc154670fe171c874a3cac4609ebc48da3a829f5
jlien11 <jlien11@hotmail.no>
2019-05-27 11:13:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -422,8 +422,7 @@ client.on('message', message => {
 						command.slice(1).forEach(word => {
 							define(word).then(returned => {
 								//client.voiceConnections.first().playFile({files: [returned.pronunciation]});
-								console.log(returned.pronunciation, returned.pronounciation);
-								client.voiceConnections.first().playFile(returned.pronunciation.toString());
+								client.voiceConnections.first().playFile(returned.pronunciation);
 							});
 						});
 					}else{
