7dcfffeb3ce8ca4f92b906c2a86ddad3b906d0b0
jlien11 <jlien11@hotmail.no>
2019-05-27 14:06:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,7 +424,7 @@ client.on('message', message => {
 								let link = returned.pronunciation;
 								console.log(link, typeof(link));
 								//client.voiceConnections.first().playArbitraryInput(returned.pronunciation);
-								client.voiceConnections.first().playArbitraryInput(link.toString());
+								client.voiceConnections.first().playArbitraryInput(link, {volume: 10});
 						}, returned => {
 								message.reply("Couldn't find that word.");
 							});
