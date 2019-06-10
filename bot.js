415ce1bcf30eea3d900d96ba72a7a15fa6ddba98
jlien11 <jlien11@hotmail.no>
2019-06-10 15:40:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,9 +107,9 @@ function getSpellThings(spellName){
 		sendhttpRequest('http://www.dnd5eapi.co/api/spells/?name='+spellName).then(returned => {
 			sendhttpRequest(returned["results"][0]["url"]).then(returned2 => {
 				resolve(formatSpellData(returned2))
-			}, returned2 => {
-				reject(returned2);
 			});
+		} , returned => {
+			reject(returned);
 		});
 	});
 }
