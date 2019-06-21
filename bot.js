2ee4ca91437e118e4cd9317992bb042691916601
jlien11 <jlien11@hotmail.no>
2019-06-21 14:23:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,6 +44,10 @@ client.on('ready', () => {
 	});
 	//set state
 	client.user.setPresence({ status: 'online', game: { name: '!help' } });
+	//check users
+	client.users.forEach(user => {
+		//do stuff to see if its in the database and update if it isn't
+	});
 });
 
 //sendhttpRequest
