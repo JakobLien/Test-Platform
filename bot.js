ac41084e2cb9bef64a6cbd4f4bfeba034606957e
jlien11 <29503509+jlien11@users.noreply.github.com>
2019-11-25 08:47:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,19 +44,17 @@ client.on('ready', () => {
 	});
 	//the weekly timer
 	let d = new Date();
-	d.setDate(d.getDate() + 8 - d.getDay());
-	d.setHours(7, 0, 0, 0);
-	let onejan = new Date(d.getFullYear(), 0, 1);
-	let week = Math.ceil((((d - onejan)/86400000) + onejan.getDay() + 1) / 7 ).toString();
-	client.setTimeout(function(week){
-		tellMe("NO KJEM DET!!!");
-		client.channels.get("636242416453812318").send("@everyone Det e no uke "+week);
-	}, d.getTime()-Date.now(), week);
-	client.setTimeout(function(week){
-		tellMe("Om to min kjem ukenummeret: "+week);
-	}, d.getTime()-Date.now()+120000, week);
-	console.log(d.getTime()-Date.now(), week);
-	
+	d.setDate(d.getDate() + (8 - d.getDay())%7);
+	if(d.getDay() !== 1 || d.getHours() <= 7){
+		d.setHours(7, 0, 0, 0);
+		let onejan = new Date(d.getFullYear(), 0, 1);
+		let week = Math.ceil((((d - onejan)/86400000) + onejan.getDay() + 1) / 7 ).toString();
+		client.setTimeout(function(week){
+			tellMe("NO KJEM DET!!!");
+			client.channels.get("636242416453812318").send("@everyone Det e no uke "+week);
+		}, d.getTime()-Date.now(), week);
+		console.log(d.getTime()-Date.now(), week);
+	}
 	client.user.setPresence({ status: 'online', game: { name: '!help' } });
 	//do stuff to see if people in the database and update if they aren't
 	runSQL("SELECT id FROM people").then(returned => {
