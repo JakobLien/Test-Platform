b7dbd4b616eb5a9920c4c017c90ac098ac54aeb9
jlien11 <29503509+jlien11@users.noreply.github.com>
2020-04-17 12:39:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -627,7 +627,6 @@ client.on("messageReactionRemove", (messageReaction, user) => {
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
-	console.log(oldState.channel, newState.channel);
 	if(oldState.id === myId && oldState.channel === null && newState.channel !== null){
 		newState.channel.join().then(voiceConnection => {
 			console.log("Successfully joined "+voiceConnection.channel.name+" on "+voiceConnection.channel.guild.name);
